083a02960e295bcbc33ec26a6a76b9107c7fe90b
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2019-03-20 22:54:02 +0100

    Preprocessing HPC

--- hpc/preprocessing.py
+++ hpc/preprocessing.py
@@ -1,11 +1,11 @@
 import os
 
 with open(os.path.join(os.pardir, "train_path.txt"), 'r') as f:
-    train_folder = f.read()
+    train_folder = f.readline()
     f.close()
 
 with open(os.path.join(os.pardir, "train_image_path.txt"), 'r') as f:
-    train_image_folder = f.read()
+    train_image_folder = f.readline()
     f.close()
 
 print(train_folder, train_image_folder)
